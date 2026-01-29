d47233ce086fc5979425ae7204d3775fb6fcc42d
Roshan Gawade <roshangawade160@gmail.com>
2026-01-30 01:03:49 +0530

    Remove scripts folder from repo (local only)

--- autozynq/scripts/activate-workflow.ts
+++
@@ -1,8 +0,0 @@
-import { activateWorkflow } from "@/lib/workflow/activation";
-
-async function main(){
-  const res = await activateWorkflow("cmk49soa00003lgth1hls37ic");
-  console.log(res);
-}
-
-main().catch(e=>{console.error(e); process.exit(1);});
--- autozynq/scripts/list-subscriptions.ts
+++
@@ -1,8 +0,0 @@
-import { getWorkflowSubscriptions } from "@/lib/triggers/subscriptions";
-
-async function main() {
-  const subs = await getWorkflowSubscriptions("cmk49soa00003lgth1hls37ic");
-  console.log(JSON.stringify(subs, null, 2));
-}
-
-main().catch((e)=>{console.error(e); process.exit(1);});
